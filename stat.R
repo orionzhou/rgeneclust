09e29cdcfe2218ff87c05b1b46ce16bf67c7d4d1
orionzhou <zhoupenggeni@gmail.com>
2016-02-24 22:21:42 -0600

    updated stat.R

--- stat.R
+++ stat.R
@@ -1,7 +1,8 @@
 require(ggplot2)
 require(plyr)
+require(VennDiagram)
 
-dirw = '/home/youngn/zhoup/git/rgeneclust/test'
+dirw = '/home/youngn/zhoup/git/rgeneclust/rosa'
 setwd(dirw)
 
 ### domain configuration
@@ -53,3 +54,29 @@ t41 = ddply(t33, .(grp), summarise, n = length(id), org = paste(sort(unique(org)
 
 fo = file.path(dirw, "52.cluster.stat.tbl")
 write.table(t33[order(t33$grp),], fo, sep = "\t", row.names = F, col.names = T, quote = F)
+
+### venn diagram for RosaR3
+tt = table(t41$org)
+a1 = tt['Fv']; a2 = tt['Md']; a3 = tt['Pp']; a12 = tt['Fv,Md']; a23 = tt['Md,Pp']; a13 = tt['Fv,Pp']; a123 = tt['Fv,Md,Pp']
+venn.plot <- draw.triple.venn(area1 = a1, area2 = a2, area3 = a3, 
+  n12 = a12+a123, n23 = a23+a123, n13 = a13+a123, n123 = a123,
+  category = c("Fv (Grape)", "Md (Apple)", "Pp (Pear)"),
+  fill = c("dodgerblue", "firebrick", "green"), lty = "solid",
+  cex = 1, cat.cex = 1, cat.dist = 0.04,
+#  cat.just = list(c(-1, -1), c(1, 1)),
+)
+fo = sprintf("%s/54.share.pdf", dirw)
+pdf(file = fo, width = 4, height = 4, bg = 'transparent')
+grid.newpage()
+grid.draw(venn.plot)
+dev.off()
+
+### AFS for RosaR12
+t42 = ddply(t33, .(grp), summarise, no = length(sort(unique(org))))
+tt = table(t42$no)
+to = data.frame(norg = as.numeric(names(tt)), cnt = as.numeric(tt))
+p = ggplot(to) +
+  geom_bar(aes(x = norg, y = cnt), stat = 'identity', width = 0.6) +
+  scale_y_continuous(name = "# NBS lineages") +
+  scale_x_continuous(name = "# sharing species", breaks = 1:6)
+ggsave(p, filename = file.path(dirw, "54.share.pdf"), width = 4, height = 4)
